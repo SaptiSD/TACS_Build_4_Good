6de5d47e3b97c062bd54a047e1c034b4fee05457
Saptarishi Das <saptarishid@outlook.com>
2025-03-29 10:04:53 -0500

    setting up initial environment

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import logging
+from dotenv import load_dotenv
+import os
+
+# Initialize Flask app with correct template folder
+app = Flask(__name__, 
+    template_folder='templates')
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+##api key example
+# api_key = os.getenv("API_KEY")
+
+# # Path to JSON data file
+# DATA_FILE = 'full file name'
+
+@app.route('/')
+def index():
+    logger.info("--- Rendering Index Page ---")
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    logger.info("--- Starting Flask App ---")
+    app.run(debug=True, port=8000)
