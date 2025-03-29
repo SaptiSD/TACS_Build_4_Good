5c4f85c04a9d1255768f9d9f3276a6243c17b021
DuxUmbra <apbhagat2006@gmail.com>
2025-03-29 12:41:59 -0500

    gemini beginning

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 import logging
 from dotenv import load_dotenv
 import os
+from google import genai
+
 
 # Initialize Flask app with correct template folder
 app = Flask(__name__, 
@@ -11,6 +13,12 @@ logger = logging.getLogger(__name__)
 
 ##api key example
 # api_key = os.getenv("API_KEY")
+load_dotenv()
+GEMINI_KEY = os.getenv("GEMINI_KEY")
+if not GEMINI_KEY:
+    raise ValueError("Gemini API key not found.")
+
+client = genai.Client(api_key = GEMINI_KEY)
 
 # # Path to JSON data file
 # DATA_FILE = 'full file name'
